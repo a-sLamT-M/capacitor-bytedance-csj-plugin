d0d1e969bc51957ec17f51be554ca78280ec8884
aslmt <megatechmuse@outlook.com>
2023-12-08 13:09:04 +0800

    Add test project Tested reward video ad version 0.0.2

---
+++ test/android/app/src/main/java/test/bytedance/csj/MainActivity.java
@@ -0,0 +1,5 @@
+package test.bytedance.csj;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ test/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.bytedance.csj',
+  appName: 'test.bytedance.csj',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
